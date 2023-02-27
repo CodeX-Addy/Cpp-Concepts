6fd4f8bb9ee6c52fa8d8c1ff9c5009ae8a07b67b
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-02-27 17:30:41 +0530

    Create doublePointers.cpp

---
+++ doublePointers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 5;
+    int *p = &i;
+    int **p2 = &p;
+    // If we want to print the content of i then there are 3 ways
+    cout << i << endl;
+    cout << *p << endl;
+    cout << **p2 << endl;
+    return 0;
+}
