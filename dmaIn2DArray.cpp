abac6882e49788df1b7d07eeb9d5fc7ea23ac481
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-03-04 19:27:35 +0530

    Update dmaIn2DArray.cpp

--- dmaIn2DArray.cpp
+++ dmaIn2DArray.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-  // Syntax for creating 2D dynamic array
-   
+    // Syntax for creating 2D dynamic array
+    int **arr = new int*[n];
+    for(int i=0; i<n; i++){
+        arr[i] = new int[n];
+    }
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cin >> arr[i][j];
