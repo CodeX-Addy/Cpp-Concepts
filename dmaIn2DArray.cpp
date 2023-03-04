02c2b64be897f9ce7892ad5f7bd0972c399d077b
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-03-04 19:28:10 +0530

    Update dmaIn2DArray.cpp

--- dmaIn2DArray.cpp
+++ dmaIn2DArray.cpp
@@ -9,11 +9,13 @@ int main() {
     for(int i=0; i<n; i++){
         arr[i] = new int[n];
     }
+    // Taking input
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cin >> arr[i][j];
         }
     }
+    // Displaying the output
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cout << arr[i][j] << " ";
