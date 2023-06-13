d33704b5cf7e4e1685e70be5aa480e1678c55047
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-06-13 22:05:43 +0530

    Create doublyLinkedlist.cpp

---
+++ doublyLinkedlist.cpp
@@ -0,0 +1,33 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* prev;
+    Node* next;
+    
+    Node(int data){
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* node1 = new Node(10);
+    Node* head = node1;
+    print(head);
+
+    return 0;
+}
