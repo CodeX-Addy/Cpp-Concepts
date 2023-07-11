d5b3ab5aecb2817021b289a39942e0028e6e8cd6
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-07-11 13:51:57 +0530

    Create circularLinkedlist.cpp

---
+++ circularLinkedlist.cpp
@@ -0,0 +1,55 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    
+    Node(int d){
+        this->data = d;
+        this->next = NULL;
+    }
+};
+
+void insert(Node* &tail, int ele, int d){
+    //If linked list is emty
+    if(tail == NULL){
+        Node* newNode = new Node(d);
+        tail = newNode;
+        newNode->next = newNode;
+    }
+    else{
+        Node* curr = tail;
+        while(curr->data != ele){
+            curr = curr->next;
+        }
+        
+        Node* temp = new Node(d);
+        temp->next = curr->next;
+        curr->next = temp;
+    }
+}
+
+void print(Node* tail){
+    Node* temp = tail;
+    do{
+        cout << tail->data << " ";
+        tail = tail->next;
+    }while(tail != temp);
+    cout << endl;
+}
+int main() {
+    Node* tail = NULL;
+    insert(tail, 3, 3);
+    print(tail);
+    
+    insert(tail, 3, 5);
+    print(tail);
+    
+    insert(tail, 5, 7);
+    print(tail);
+
+    return 0;
+}
