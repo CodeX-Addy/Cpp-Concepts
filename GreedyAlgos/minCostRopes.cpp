539658a4ce4f2d491225093b43a8a664dae5b9d0
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-03-16 12:15:33 +0530

    .

--- GreedyAlgos/minCostRopes.cpp
+++ GreedyAlgos/minCostRopes.cpp
@@ -3,21 +3,22 @@
 #include<queue>
 using namespace std;
 
-int minCost(vector<int> &v){
+int minCost(vector<int>&v){
     priority_queue<int, vector<int>, greater<int>> minHeap;
     for(int i=0; i<v.size(); ++i){
         minHeap.push(v[i]);
     }
+
     int cost = 0;
-    while(minHeap.size() > 1){
-        int a = minHeap.top();
+    while(minHeap.size()>1){
+        int f = minHeap.top();
         minHeap.pop();
 
-        int b = minHeap.top();
+        int s = minHeap.top();
         minHeap.pop();
 
-        cost += a + b;
-        int sum = a + b;
+        cost+=f+s;
+        int sum=f+s;
         minHeap.push(sum);
     }
     return cost;
