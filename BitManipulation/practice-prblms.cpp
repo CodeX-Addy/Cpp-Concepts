20fea5afc0b3a8c521359dd7dcf2cf165cf8192a
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-03-17 14:21:44 +0530

    .

--- BitManipulation/practice-prblms.cpp
+++ BitManipulation/practice-prblms.cpp
@@ -8,9 +8,19 @@ void checkEvenOdd(int&n){
         cout << "Even" << endl;
 }
 
+void getithBit(int n, int i){
+    int mask = 1<<i;
+    int ans = n&mask;
+    if(ans)
+        cout << "Bit is->1" << endl;
+    else
+        cout << "Bit is->0" << endl;
+}
+
 int main(){
     int n;
-    cin >> n;
-    checkEvenOdd(n);
+    //cin >> n;
+    //checkEvenOdd(n);
+    getithBit(4, 1);
     return 0;
 }
